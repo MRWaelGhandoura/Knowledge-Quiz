deb00243e609067da5fcb2a04a6fae3c003db03f
MRWaelGhandoura <waelghandouranew@gmail.com>
2022-04-23 23:13:08 +0000

    adding js questions to a new js page

---
+++ assets/js/questions-page.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+      question: 'What is the atomic sign for water on the periodic table ?',
+      answers: [
+        { text: 'H2O', correct: true },
+        { text: 'O2', correct: false },
+        { text: 'CO2', correct:false},
+        { text: 'He', correct:false},
+
+      ]
+    },
+    {
+      question: 'What is the Currency of Sweden?',
+      answers: [
+        { text: 'Krona', correct: true },
+        { text: 'Pound Sterling', correct: false },
+        { text: 'Yen', correct: false },
+        { text: 'Euro', correct: false }
+      ]
+    },
+    {
+      question: 'What is the largest animal in the world ?',
+      answers: [
+        { text: 'Giraffe', correct: false },
+        { text: 'Whale', correct: true },
+        { text: 'Elephant', correct: false },
+        { text: 'Anaconda', correct: false }
+      ]
+    },
+    {
+        question: 'What is the longest River in the World ?',
+        answers: [
+          { text: 'Nile', correct: false },
+          { text: 'Amazon', correct: true },
+          { text: 'Mississippi', correct: false },
+          { text: 'Yellow River', correct: false }
+        ]
+      },
+    {
+      question: 'Who painted Mona Lisa?',
+      answers: [
+        { text: 'Michel Angelo', correct: false },
+        { text: 'Leonardo de Vinci', correct: true },
+        { text: 'Salvador Dali', correct: false },
+        { text: 'Vincent Van Gogh', correct: false }
+      ]
+    }
+  ]
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
 function startGame() {
 
+    startButton.classList.add('hide')
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -7,5 +16,5 @@ function setNextQuestion() {
 }
 
 function selectAnswer() {
-    
+
 }
